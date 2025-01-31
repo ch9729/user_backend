e2449263fffeb43d0cec86bdb97eae314eac7c34
ch9729 <karlll@naver.com>
2025-01-31 15:55:40 +0900

    user 추가

--- src/main/java/com/mysite/backend/controller/UserController.java
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -3,11 +3,12 @@ package com.mysite.backend.controller;
 import com.mysite.backend.model.User;
 import com.mysite.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
@@ -16,5 +17,10 @@ public class UserController {
     @PostMapping("/users")
     User addUser(@RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
