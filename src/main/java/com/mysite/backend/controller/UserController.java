f7949fc0d8400ee1f380e182c1ff7cac3eb5e94b
ch9729 <karlll@naver.com>
2025-02-03 11:45:15 +0900

    유저 정보수정

--- src/main/java/com/mysite/backend/controller/UserController.java
+++ src/main/java/com/mysite/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.mysite.backend.controller;
 
+import com.mysite.backend.exception.UserNotFoundException;
 import com.mysite.backend.model.User;
 import com.mysite.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,26 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/users/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        // return userRepository.findById(id).map(user -> {
+        //     user.setUsername(newUser.getUsername());
+        //     user.setName(newUser.getName());
+        //     user.setEmail(newUser.getEmail());
+        //     return userRepository.save(user);
+        // }).orElseThrow(() -> new UserNotFoundException(id));
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+        // 찾은 유저에 새유저 내용으로 변경
+        user.setUsername(newUser.getUsername());
+        user.setName(newUser.getName());
+        user.setEmail(newUser.getEmail());
+        return userRepository.save(user);
     }
 }
